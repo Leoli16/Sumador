90f1fa45cecda09655e13f6ecb86b569a53c5908
Leonel Abud <abudleito@gmail.com>
2025-05-07 15:57:40 -0300

    Divididor

---
+++ Assets/divididor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class divididor : MonoBehaviour
+{
+    public TMP_InputField num1;
+    public TMP_InputField num2;
+    public TextMeshProUGUI resultado;
+    
+    public void dividir()
+    {
+        int numero1 = int.Parse(num1.text);
+        int numero2 = int.Parse(num2.text);
+        if (numero2 != 0)
+        {
+            resultado.text = (numero1 / numero2).ToString();
+        }
+        else
+        {
+            resultado.text = "¡No se puede dividir por 0!";
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
